f6777711f31c36d1e53268ba4c4ec3662f731407
antoine <chahine.antoine@aut.edu>
2023-01-13 09:45:27 +0200

    PLeaseeeee

--- api-test.js
+++ api-test.js
@@ -23,9 +23,6 @@ function getDatafromAPI() {
         const url = "https://stablediffusionapi.com/api/sandbox/v3/fine_tune";
         (0, cross_fetch_1.default)(url, {
             method: "POST",
-            headers: {
-                "X-Auth-Token": "lUHfz6htjXph9Is7ZkuoIp218ZFbs1eqQRgI7MI9SuZR7ULbGR8XUZAhgYBh",
-            },
         })
             .then((response) => response.json())
             .then((data) => console.log(data));
--- api-test.ts
+++ api-test.ts
@@ -7,10 +7,6 @@ export async function getDatafromAPI() {
   const url = "https://stablediffusionapi.com/api/sandbox/v3/fine_tune";
   fetch(url, {
     method: "POST",
-    headers: {
-      "X-Auth-Token":
-        "lUHfz6htjXph9Is7ZkuoIp218ZFbs1eqQRgI7MI9SuZR7ULbGR8XUZAhgYBh",
-    },
   })
     .then((response) => response.json())
     .then((data) => console.log(data));
