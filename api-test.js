12c6cd60c6487c249b2ff94cc0d72b733b85bf94
antoine <chahine.antoine@aut.edu>
2023-01-13 09:03:58 +0200

    PLeaseeeee

--- api-test.js
+++ api-test.js
@@ -1,7 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.antoine = void 0;
+exports.getDatafromAPI = exports.antoine = void 0;
 function antoine() {
     return "hi from antoine";
 }
 exports.antoine = antoine;
+function getDatafromAPI() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("https://stablediffusionapi.com/api/sandbox/v3/fine_tune");
+        const data = yield response.json();
+        console.log(data);
+        return data;
+    });
+}
+exports.getDatafromAPI = getDatafromAPI;
--- api-test.ts
+++ api-test.ts
@@ -1,3 +1,12 @@
 export function antoine(): string {
   return "hi from antoine";
 }
+
+export async function getDatafromAPI() {
+  const response = await fetch(
+    "https://stablediffusionapi.com/api/sandbox/v3/fine_tune"
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
